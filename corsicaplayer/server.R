540a16a5858136914fdebff296016ea1d1db86c2
Manny <Manny@Emmanuels-Mac-mini.local>
2016-01-23 11:07:27 -0500

    1/23/2016

--- corsicaplayer/server.R
+++ corsicaplayer/server.R
@@ -13,7 +13,7 @@ library(DT)
 
 # Load data
 #require(httr)
-setwd("~/shiny-server/corsicaplayer/")
+setwd("~/srv/shiny-server/corsicaplayer/")
 #response <- GET(url = "https://dl.dropbox.com/s/qx4ywngp7jh17ei/playertest.Rda?dl=1")
 #writeBin(response$content, "test2.Rda")
 load("playertest.Rda")
